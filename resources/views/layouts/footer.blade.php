f879ca7b7f493113d482a23a40b20df128008a3d
Ricardo Aguilar <reacxd579@gmail.com>
2023-11-06 23:11:26 -0600

    footer actualizado

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,7 @@
 <footer>
-
+    <div class="bg-gray-800 py-4">
+        <div class="container flex items-center justify-between">
+            <p class="text-white">&copy; E-Commerce - 2023</p>
+        </div>
+    </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -234,7 +234,14 @@
                 </div>
             </div>
         </div>
-
-
     </body>
+@endsection
+@section('footer')
+<footer>
+    <div class="bg-gray-800 py-4">
+        <div class="container flex items-center justify-between">
+            <p class="text-white">&copy; E-Commerce - 2023</p>
+        </div>
+    </div>
+</footer>
 @endsection
