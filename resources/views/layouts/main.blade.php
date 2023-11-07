3c50d8b2ae3d4e8161aa2446147c48b2b0f78ab4
Elian Alvarado <elian.alvarado.2004@gmail.com>
2023-11-06 19:15:02 -0600

    Implementar avances

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,2 +1,19 @@
 <x-header/>
-<h1>hola mundo</h1>
+
+<body>
+    
+    {{-- Banner --}}
+    <div class="bg-cover bg-no-repeat bg-center py-36" style="background-image: url('{{ asset('img//backgrounds/banner-bg.jpg') }}');">
+        <div class="container">
+            <h1 class="text-6xl text-white font-medium mb-4 capitalize">
+                BIENVENIDOS <br> XDDDDDD
+            </h1>
+            <p class="text-white">UN IPHONEEEEEEEEEEEEEEEEEEEEEEEEE</p>
+            <div class="mt-12">
+                <a href="#" class="bg-primary border border-primary text-white px-8 py-3 font-medium 
+                    rounded-md hover:bg-transparent hover:text-primary">LO NECESITO!!!</a>
+            </div>
+        </div>
+    </div>
+
+</body>
