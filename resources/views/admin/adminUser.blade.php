be5d45a5e3f9d88e4ef2161161d9f7560a158354
Yarissad <145793443+Yarissad@users.noreply.github.com>
2023-11-09 03:03:27 -0600

    Actualización adminUser

--- resources/views/admin/adminUser.blade.php
+++ resources/views/admin/adminUser.blade.php
@@ -110,66 +110,205 @@
                     </ul>
                 </div>
             </div>
+
+            <button type="button" data-modal-target="medium-modal" data-modal-toggle="medium-modal"
+                class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7">Agregar
+                Usuario</button>
         </div>
         {{-- AQUÍ TERMINA EL DISEÑO PARA FILTRAR POR ROL --}}
 
         <div class="h-96 overflow-y-scroll">
-        <table class="w-full text-sm text-left text-black dark:text-black ">
-            <thead class="text-xs text-black uppercase bg-gray-50 dark:bg-gray-700 dark:text-black sticky top-0">
-                <tr>
+            <table class="w-full text-sm text-left text-black dark:text-black ">
+                <thead class="text-xs text-black uppercase bg-gray-50 dark:bg-gray-700 dark:text-black sticky top-0">
+                    <tr>
 
-                    <th scope="col" class="px-6 py-3">
-                        ID
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        NOMBRE
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        EMAIL
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        ROL
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        ESTADO
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        ACTION
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($usuarios as $usuario)
-                    <tr
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{ $usuario->id }}
+                        <th scope="col" class="px-6 py-3">
+                            ID
                         </th>
-                        <td class="px-6 py-4">
-                            {{ $usuario->name }}
-                        </td>
-
-                        <td class="px-6 py-4">
-                            {{ $usuario->email }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $usuario->role->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $usuario->estado->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="#"
-                                class="font-medium text-3xl  text-blue-600 dark:text-blue-500 hover:underline"><i class="fa-solid fa-pen-to-square" style="color: #0056c7;"></i></a>
-                            <a href="#"
-                                class="font-medium text-3xl text-red-600 dark:text-red-600  hover:underline"><i class="fa-solid fa-trash-can" style="color: #fe0101;"></i></a>
-
-                        </td>
+                        <th scope="col" class="px-6 py-3">
+                            NOMBRE
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            EMAIL
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ROL
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ESTADO
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ACTION
+                        </th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($usuarios as $usuario)
+                        <tr
+                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $usuario->id }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $usuario->name }}
+                            </td>
+
+                            <td class="px-6 py-4">
+                                {{ $usuario->email }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $usuario->role->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $usuario->estado->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <a href="#" data-modal-target="small-modal" data-modal-toggle="small-modal"
+                                    class="font-medium text-3xl  text-blue-600 dark:text-blue-500 hover:underline"><i
+                                        class="fa-solid fa-pen-to-square" style="color: #0056c7;"></i></a>
+                                <a href="#"
+                                    class="font-medium text-3xl text-red-600 dark:text-red-600  hover:underline"><i
+                                        class="fa-solid fa-trash-can" style="color: #fe0101;"></i></a>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+
+    {{-- MODAL PARA AGREGAR UN NUEVO USUARIO --}}
+    <!-- Default Modal -->
+    <div id="medium-modal" tabindex="-1"
+        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-lg max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                        Default modal
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="medium-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        With less than a month to go before the European Union enacts new consumer privacy laws for its
+                        citizens, companies around the world are updating their terms of service agreements to comply.
+                    </p>
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is
+                        meant to ensure a common set of data rights in the European Union. It requires organizations to
+                        notify users as soon as possible of high-risk data breaches that could personally affect them.
+                    </p>
+                </div>
+                <!-- Modal footer -->
+                <div
+                    class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button data-modal-hide="medium-modal" type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Guardar</button>
+                    <button data-modal-hide="medium-modal" type="button"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- MODAL PARA EDITAR EL USUARIO --}}
+    <!-- Small Modal -->
+    
+    <div id="small-modal" tabindex="-1"
+        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                        Editar Usuario
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="small-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+
+                    <div class="flex">
+                        <span
+                            class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                            <svg class="w-4 h-4 text-black dark:text-black" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                            </svg>
+                        </span>
+                        <input type="text" id="website-admin" value=""
+                            class="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Nombre" >
+                            
+                    </div>
+                    <div class="flex">
+                        <span
+                            class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                            <i class="fa-solid fa-envelope"></i>
+                        </span>
+                        <input type="text" id="website-admin" value=""
+                            class="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Correo" >
+                            
+                    </div>
+                    <div class="flex flex-grow">
+                    <div class="flex flex-row">
+                        
+                        <select id="rol" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-48  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Rol</option>
+                            <option value="admin">Admin</option>
+                            <option value="cliente">Cliente</option>
+                          </select>
+                            
+                    </div>
+                    <div class="flex flex-row ml-3">
+                        <select id="estado" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-48  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Estado</option>
+                            <option value="admin">Activo</option>
+                            <option value="cliente">Inactivo</option>
+                          </select>
+                            
+                    </div>
+                </div>
+
+                </div>
+                <!-- Modal footer -->
+                <div
+                    class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button data-modal-hide="small-modal" type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Actualizar</button>
+                    <button data-modal-hide="small-modal" type="button"
+                        class="text-white bg-red-600  hover:bg-red-700  focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-white focus:z-10 dark:bg-red-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-red-700  dark:focus:ring-red-500">Cerrar</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
