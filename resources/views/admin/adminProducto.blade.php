34a4f6f7e0a24b2e60004cbab67cbac0bfa43824
Yarissad <145793443+Yarissad@users.noreply.github.com>
2023-11-09 03:04:13 -0600

    Actualización adminProducto

--- resources/views/admin/adminProducto.blade.php
+++ resources/views/admin/adminProducto.blade.php
@@ -2,77 +2,232 @@
 
 @section('contenido')
     @extends('components.header')
-    <a href="{{ route('create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4">Agregar Producto</a>
-    <div class="w-full h-[680px] flex items-center justify-center">
-    
-        <div class="block w-full overflow-x-auto max-w-xl border">
-            <table class="items-center w-full bg-transparent border-collapse">
-                <thead>
-                    <tr>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            id</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            name</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            descripcion</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            imagen</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            categoria</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            precio</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            stock</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            estado</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-                            Actions</th>
-                        <th
-                            class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-100">
-                    @foreach ($productos as $producto)
-                        <tr class="text-gray-500">
-                            <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">
-                                {{ $producto->id }}</th>
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->name }}</td>
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->descripcion }}</td>
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->imagen }}</td>
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->categoria_id }}</td>
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->precio }}</td>
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->stock }}</td>
-                            
-                            <td
-                                class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
-                                {{ $producto->estado_id }}</td>
-                        </tr>
-                    @endforeach
 
-                </tbody>
-            </table>
+    <div class="relative mt-2 p-10 overflow-x-auto shadow-md sm:rounded-lg ">
+
+        <div class=" pb-5 relative flex flex-row">
+            <div class="absolute -mt-4 inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </div>
+            <input type="text" id="table-search"
+                class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-3/4    bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Buscar por nombre">
+
+            {{-- AQUÍ COMIENZA EL DISEÑO PARA FILTRAR POR CATEGORÍA  --}}
+            <div class="flex flex-row ml-3">
+
+                <button id="dropdownRadioButton" data-dropdown-toggle="dropdownDefaultRadio"
+                    class="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="button">
+                    Buscar por categoría
+                    <svg class="w-2.5 h-2.5 ml-36  " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 4 4 4-4" />
+                    </svg>
+                </button>
+
+                <!-- Dropdown menu -->
+                <div id="dropdownDefaultRadio"
+                    class="z-10 w-72  hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+
+                    <ul class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
+                        aria-labelledby="dropdownRadioButton">
+                        <li>
+                            <div class="flex items-center">
+                                <input id="default-radio-1" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-1"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">TV y Video</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-2"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Celulares</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-2"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Computación</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-2"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Celulares</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-2"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Electrodomésticos</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-2"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Prendas Electrónicas</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="default-radio-2"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Audífonos</label>
+                            </div>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+
+            </div>
         </div>
+        {{-- AQUÍ TERMINA EL DISEÑO PARA FILTRAR POR CATEGORÍA     --}}
+
+        {{-- AQUÍ COMIENZA EL DISEÑO PARA FILTRAR POR ESTADO --}}
+        <div class="flex items-center justify-between pb-4">
+            <div>
+                <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio"
+                    class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    type="button">
+                    <svg class="w-3 h-3 text-gray-500 dark:text-gray-400 mr-2.5" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path
+                            d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+                    </svg>
+                    ESTADO
+                    <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 4 4 4-4" />
+                    </svg>
+                </button>
+                <!-- Dropdown menu -->
+                <div id="dropdownRadio"
+                    class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                    data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top"
+                    style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);">
+                    <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+                        aria-labelledby="dropdownRadioButton">
+                        <li>
+                            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input id="filter-radio-example-1" type="radio" value="" name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-1"
+                                    class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Activo</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input checked="" id="filter-radio-example-2" type="radio" value=""
+                                    name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-2"
+                                    class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Inactivo</label>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        {{-- AQUÍ TERMINA EL DISEÑO PARA FILTRAR POR ESTADO --}}
+
+        <div class="h-96 overflow-y-scroll">
+        <table class="w-full text-sm text-left text-black dark:text-black ">
+            <thead class="text-xs text-black uppercase bg-gray-50 dark:bg-gray-700 dark:text-black sticky top-0">
+                <tr>
+
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        NAME
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        DESCRIPCIÓN
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        IMAGEN
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        CATEGORÍA
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        PRECIO
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        STOCK
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        ESTADO
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        ACTION
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($productos as $producto)
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $producto->id }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $producto->name }}
+                        </td>
+
+                        <td class="px-6 py-4">
+                            {{ $producto->descripcion }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $producto->imagen }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $producto->categoria_id }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $producto->precio }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $producto->stock }}</td>
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $producto->estado_id }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#"
+                                class="font-medium text-3xl  text-blue-600 dark:text-blue-500 hover:underline"><i class="fa-solid fa-pen-to-square" style="color: #0056c7;"></i></a>
+                            <a href="#"
+                                class="font-medium text-3xl text-red-600 dark:text-red-600  hover:underline"><i class="fa-solid fa-trash-can" style="color: #fe0101;"></i></a>
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     </div>
 @endsection
